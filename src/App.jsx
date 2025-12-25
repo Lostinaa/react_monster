39e9e5335e9fe58b04e89367d0ec5753973eee62
lostinaa <lostinaa@github.com>
2025-12-25 14:58:47 +0300

    strike

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useMemo } from 'react'
 import './App.css'
+import CardList from './components/CardList'
+import SearchBox from './components/SearchBox'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [monsters, setMonsters] = useState([])
+  const [searchField, setSearchField] = useState('')
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users))
+  }, [])
+
+  const filteredMonsters = useMemo(() => {
+    return monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+  }, [monsters, searchField])
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase()
+    setSearchField(searchFieldString)
+  }
 
   return (
-    <>
-     <h1> cyclone got emmmm
-     </h1>
-    </>
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        className="monsters-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
   )
 }
 
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,15 @@
+import './SearchBox.css'
+
+const SearchBox = ({ onChangeHandler, placeholder, className }) => {
+  return (
+    <input
+      className={`search-box ${className}`}
+      type="search"
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+    />
+  )
+}
+
+export default SearchBox
+
